e4f8d29438fda20fc1619a62c2273e7ce46c0569
yaedavid <83503728+yaedavid@users.noreply.github.com>
2023-09-10 23:06:15 +0300

    Add files via upload

---
+++ 2020-2021/os-intro/labs/lab12/prog3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+opt=$1;
+form=$2;
+num=$3;
+function Files() {
+    for ((i=1; i<=$num; i++)) do
+	file=$(echo $form | tr '#' "$i")
+	if [ $opt == "-r" ]
+	then
+	    rm -f $file
+	elif [ $opt == "-c" ]
+	then
+	    touch $file
+	fi
+    done
+}
+Files
